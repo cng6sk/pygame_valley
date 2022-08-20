cd7ab5a94130151517f391a3c6c6724b5aa078f9
cng6sk <a838928964@gmail.com>
2022-08-20 23:44:19 +0800

    l1_setup

--- code/main.py
+++ code/main.py
@@ -2,13 +2,16 @@ import pygame
 import sys
 
 from settings import *
+from level import Level
 
 
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption('Game')
         self.clock = pygame.time.Clock()
+        self.level = Level()
 
     def run(self):
         while True:
@@ -18,6 +21,9 @@ class Game:
                     sys.exit()
 
             dt = self.clock.tick() / 1000
+            # before updating display
+            self.level.run(dt)
+            # updating display
             pygame.display.update()
 
 
