a9e4668ab14dc3bf807c2b0cee6bdd607b6e05ab
cng6sk <a838928964@gmail.com>
2022-08-21 13:54:51 +0800

    l3_import the player graphics

--- code/player.py
+++ code/player.py
@@ -1,13 +1,18 @@
 import pygame
 from settings import *
+from support import *
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
 
+        self.import_assets()
+        self.status = 'down_idle'
+        self.frame_index = 0
+
         # general setup
-        self.image = pygame.Surface((32, 64))
+        self.image = self.animations[self.status][self.frame_index]
         self.image.fill('green')
         self.rect = self.image.get_rect(center=pos)
 
@@ -15,6 +20,16 @@ class Player(pygame.sprite.Sprite):
         self.direction = pygame.math.Vector2()
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
+
+    def import_assets(self):
+        self.animations = {'up': [], 'down': [], 'left': [], 'right': [],
+                           'right_idle': [], 'left_idle': [], 'up_idle': [], 'down_idle': [],
+                           'right_hoe': [], 'left_hoe': [], 'up_hoe': [], 'down_hoe': [],
+                           'right_axe': [], 'left_axe': [], 'up_axe': [], 'down_axe': [],
+                           'right_water': [], 'left_water': [], 'up_water': [], 'down_water': []}
+        for animation in self.animations.keys():
+            full_path = '../graphics/character/' + animation
+            self.animations[animation] = import_folder(full_path)
 
     def input(self):
         keys = pygame.key.get_pressed()
